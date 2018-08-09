2e70ae6d802eb009b99eaed8002da09efae66486
claudio <claudioaguilar2000@icloud.com>
2018-08-09 16:42:25 -0400

    Updated settings.py

--- STEMeats/settings.py
+++ STEMeats/settings.py
@@ -31,7 +31,7 @@ CSRF_COOKIE_SECURE = True
 
 SESSION_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = ['cryptic-springs-74775.herokuapp.com/', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.herokuapp.com/', '127.0.0.1', 'localhost']
 
 
 # Application definition
